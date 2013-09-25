a123d2ec8edc93607ee36e7e24935d79b6f458b7
Zach <brantmeierz@ww-whippets.org>
2013-09-24 20:14:46 -0500

    Added pi, tau, exponent, and improvements

--- mathFunc.js
+++ mathFunc.js
@@ -1,6 +1,11 @@
+var pi=Math.PI;
+var tau=Math.PI*2;
 function distance(xa,ya,xb,yb){
   return sqrt(Math.pow(xa-xb,2)+Math.pow(ya-yb,2);
 }
 function midpoint(xa,ya,xb,yb){
   return "("+(xa+xb)/2+","+(ya+yb)+")";
+}
+function exponent(num,pow){
+  return Math.pow(num,pow);
 }
