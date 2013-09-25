5c6d2b3906bc38dbb3573f7f40c0afabd1961ab1
Zach <brantmeierz@ww-whippets.org>
2013-09-24 20:54:30 -0500

    Many improvements and additions

--- mathFunc.js
+++ mathFunc.js
@@ -1,11 +1,35 @@
-var pi=Math.PI;
-var tau=Math.PI*2;
+var isOpera = !!window.opera || navigator.userAgent.indexOf(' OPR/') >= 0;
+var isFirefox = typeof InstallTrigger !== 'undefined';
+var isSafari = Object.prototype.toString.call(window.HTMLElement).indexOf('Constructor') > 0;
+var isChrome = !!window.chrome && !isOpera;
+var isIE = /*@cc_on!@*/false || document.documentMode;
+if(isChrome||isFirefox){
+  const PI=Math.PI;
+  const TAU=Math.PI*2;
+  const E=Math.E;
+}else{
+  var PI=Math.PI;
+  var TAU=Math.PI*2;
+  var E=Math.E;
+}
+function cosine(num){
+  return Math.cos(num);
+}
 function distance(xa,ya,xb,yb){
   return sqrt(Math.pow(xa-xb,2)+Math.pow(ya-yb,2);
+}
+function exponent(num,pow){
+  return Math.pow(num,pow);
 }
 function midpoint(xa,ya,xb,yb){
   return "("+(xa+xb)/2+","+(ya+yb)+")";
 }
-function exponent(num,pow){
-  return Math.pow(num,pow);
+function round(num){
+  return Math.round(num);
+}
+function sine(num){
+  return Math.sin(num);
+}
+function tangent(num){
+  return Math.tan(num);
 }
