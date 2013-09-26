bed1f3e0d48005ef5574d9944a3d9ee2ed4ec8f2
Zach <brantmeierz@ww-whippets.org>
2013-09-25 20:15:48 -0500

    Changes and additions

--- mathFunc.js
+++ mathFunc.js
@@ -1,22 +1,11 @@
-var isOpera = !!window.opera || navigator.userAgent.indexOf(' OPR/') >= 0;
-var isFirefox = typeof InstallTrigger !== 'undefined';
-var isSafari = Object.prototype.toString.call(window.HTMLElement).indexOf('Constructor') > 0;
-var isChrome = !!window.chrome && !isOpera;
-var isIE = /*@cc_on!@*/false || document.documentMode;
-if(isChrome||isFirefox){
-  const PI=Math.PI;
-  const TAU=Math.PI*2;
-  const E=Math.E;
-}else{
-  var PI=Math.PI;
-  var TAU=Math.PI*2;
-  var E=Math.E;
-}
 function cosine(num){
   return Math.cos(num);
 }
 function distance(xa,ya,xb,yb){
   return sqrt(Math.pow(xa-xb,2)+Math.pow(ya-yb,2);
+}
+function equilateralTriangleArea(side){
+  return (Math.pow(side,2)*Math.sqrt(3))/4;
 }
 function exponent(num,pow){
   return Math.pow(num,pow);
@@ -32,4 +21,21 @@ function sine(num){
 }
 function tangent(num){
   return Math.tan(num);
+}
+function triangleArea(base,height){
+  return base*0.5*height;
+}
+var isOpera = !!window.opera || navigator.userAgent.indexOf(' OPR/') >= 0;
+var isFirefox = typeof InstallTrigger !== 'undefined';
+var isSafari = Object.prototype.toString.call(window.HTMLElement).indexOf('Constructor') > 0;
+var isChrome = !!window.chrome && !isOpera;
+var isIE = /*@cc_on!@*/false || document.documentMode;
+if(isChrome||isFirefox){
+  const PI=Math.PI;
+  const TAU=Math.PI*2;
+  const E=Math.E;
+}else{
+  var PI=Math.PI;
+  var TAU=Math.PI*2;
+  var E=Math.E;
 }
